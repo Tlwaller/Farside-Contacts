ef7d9053580a6432041e4a8bc2866c37fc6867ad
Trey Waller <tlwaller99@gmail.com>
2021-12-09 10:47:58 -0600

    add client form closes on outside click

--- src/Components/AddClientForm.js
+++ src/Components/AddClientForm.js
@@ -14,29 +14,31 @@ export default class AddClientForm extends Component {
         };
     };
 
-    toggleForm = e => {
-        // e.preventDefault();
-        if (this.state.display === 'inline') {
+    toggleForm = () => {
+        if (this.state.display === 'flex') {
             this.setState({display: 'none'});
-        } else this.setState({display: 'inline'})
+        } else this.setState({display: 'flex'})
     }
 
     render() {
         return (
-            <div className="add-client-form-container" style={{display: this.state.display}}>
-                <form>
-                    <div className="acf-head">
-                        <input className="acf-name" autoFocus type='text' value={this.state.name} onFocus={e => e.target.select()} onChange={e => this.setState({name: e.target.value})}/>
-                        <i className="fas fa-times acf-close" onClick={this.toggleForm}/>
-                    </div>
-                    
-                    <input className="acf-input" type='text' placeholder="address" onChange={e => this.setState({address: e.target.value})}/>
-                    <input className="acf-input" type='text' placeholder="website" onChange={e => this.setState({website: e.target.value})}/>
-                    <input className="acf-input" type='text' placeholder="phone" onChange={e => this.setState({phone: e.target.value})}/>
-                    <input className="acf-input" type='text' placeholder="primary contact" onChange={e => this.setState({primaryContact: e.target.value})}/>
-                    <input className="acf-input" type='text' placeholder="additional contacts" onChange={e => this.setState({additionalContacts: e.target.value})}/>           
-                </form>
+            <div className="outside-click-div" onClick={e => {e.preventDefault(); this.toggleForm();}} style={{display: this.state.display}}>
+                <div className="add-client-form-container" onClick={e => e.stopPropagation()}>
+                    <form>
+                        <div className="acf-head">
+                            <input className="acf-name" autoFocus type='text' value={this.state.name} onFocus={e => e.target.select()} onChange={e => this.setState({name: e.target.value})}/>
+                            <i className="fas fa-times acf-close" onClick={this.toggleForm}/>
+                        </div>
+                        
+                        <input className="acf-input" type='text' placeholder="address" onChange={e => this.setState({address: e.target.value})}/>
+                        <input className="acf-input" type='text' placeholder="website" onChange={e => this.setState({website: e.target.value})}/>
+                        <input className="acf-input" type='text' placeholder="phone" onChange={e => this.setState({phone: e.target.value})}/>
+                        <input className="acf-input" type='text' placeholder="primary contact" onChange={e => this.setState({primaryContact: e.target.value})}/>
+                        <input className="acf-input" type='text' placeholder="additional contacts" onChange={e => this.setState({additionalContacts: e.target.value})}/>           
+                    </form>
+                </div>
             </div>
+            
         )
     }
 }
